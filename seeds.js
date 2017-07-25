aa2e09e3fcea9c4d8d11bab9bd16e5f1e3a53269
Ariel Blasi <exorcismo@gmail.com>
2017-07-25 19:42:03 -0300

    styling comments

--- seeds.js
+++ seeds.js
@@ -3,12 +3,12 @@ const Campground = require("./models/campground")
 const Comment = require("./models/comment")
 
 var data = [
-	{name:"Rosario", image:"https://farm4.staticflickr.com/3062/2984119099_82336dfc3b.jpg",
-		description:"This campground is great for campfires and tents, but don't be tempted...ha"},
+	{name:"Rosario del Tala", image:"https://farm4.staticflickr.com/3062/2984119099_82336dfc3b.jpg",
+		description:"This campground is great for campfires and tents, but don't be tempted...ha. Here's a bunch of numbers. They may look random but they're my phone number. I'm no hero. I just put my bra on."},
 	{name:"Victoria", image:"https://farm7.staticflickr.com/6103/6333668591_90e7c2bc72.jpg",
-		description:"This campground is great for campfires and tents, but don't be tempted...ha"},
+		description:"This campground is great for campfires and tents, but don't be tempted...ha. Here's a bunch of numbers. They may look random but they're my phone number. I'm no hero. I just put my bra on."},
 	{name:"Colastiné", image:"https://farm4.staticflickr.com/3393/3510641019_bc91eb6818.jpg",
-		description:"This campground is great for campfires and tents, but don't be tempted...ha"}
+		description:"This campground is great for campfires and tents, but don't be tempted...ha. Here's a bunch of numbers. They may look random but they're my phone number. I'm no hero. I just put my bra on."}
 ]
 
 function seedDB(){
